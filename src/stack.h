36f8f4080e78855d4400a3f42f5a21992e99e9c5
Kyle Amoroso <kamoroso1994@gmail.com>
2018-10-28 03:19:39 -0400

    Fix stack_create name inconsistency

--- src/stack.h
+++ src/stack.h
@@ -10,7 +10,7 @@ typedef struct {
   int elsize;
 } stack_t;
 
-int stack_init(stack_t *stack, int elsize);
+int stack_create(stack_t *stack, int elsize);
 int stack_push(stack_t *stack, const void *item);
 int stack_pop(stack_t *stack, void *item);
 void stack_destroy(stack_t *stack);
